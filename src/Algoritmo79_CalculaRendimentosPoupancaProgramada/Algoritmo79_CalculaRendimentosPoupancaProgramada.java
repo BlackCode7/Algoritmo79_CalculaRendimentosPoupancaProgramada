1e2d411662536b5ff5155f3ed96030fffbd804fc
Anderson <BlackCode7@outlook.com>
2022-06-20 21:38:17 -0300

    commit create algorithm count

--- src/Algoritmo79_CalculaRendimentosPoupancaProgramada/Algoritmo79_CalculaRendimentosPoupancaProgramada.java
+++ src/Algoritmo79_CalculaRendimentosPoupancaProgramada/Algoritmo79_CalculaRendimentosPoupancaProgramada.java
@@ -1,10 +1,34 @@
 package Algoritmo79_CalculaRendimentosPoupancaProgramada;
 
+import java.util.Scanner;
+
 public class Algoritmo79_CalculaRendimentosPoupancaProgramada {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		extracted();
+		
+		
+	}
+
+	public static void extracted() {
+		double va, i, p;
+		int n;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite o valor da Aplicação: ");
+		p = scanner.nextDouble();
+		System.out.println("Digite a taxa ( 0 - 1 ): ");
+		i = scanner.nextDouble();
+		System.out.println("Digite o número de meses: ");
+		n = scanner.nextInt();
+		
+		double poten = ( 1 + i );
+		
+		va = p * ( Math.pow(poten,  n) - 1 ) / i;
+		
+		System.out.println("Valor acumulado: " + va);
 	}
 
 }
